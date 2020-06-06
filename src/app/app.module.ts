ec85fce34a3739669c332d827f6ab4e68d9ef89a
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-06 14:09:11 +0300

    added post actions and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+
 import { simpleReducer } from './simple.reducer';
+import { postReducer } from './reducers/post.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +16,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ message: simpleReducer })
+    StoreModule.forRoot({
+      post: postMessage,
+      message: simpleReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
